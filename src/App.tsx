29e2be3a0886ee01ad80432ed45615ca5f56bdb9
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-04 15:21:52 +0200

    adding a 'champion pool' and displaying it on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { characters } from './characters'
+
+
+
+
+
+
 
 function App() {
 
+  
+
+  const arr = []
+  
+  const characterImgs = []
+  for (const character of characters) {
+    for (const role of Object.values(character)) {
+      for (const image of Object.values(role)) {
+        if (image.endsWith(".png")) {
+          characterImgs.push(image)
+        }
+      }
+    }
+  }
+
+  console.log(characterImgs)
   return (
     <div className="App">
         <h1>Autochess board</h1>
+        <div className='characters'>
+          {characterImgs.map((characterImg, idx) => (
+            <div key={idx} draggable={true} className='character-img'>
+              {characterImg ? <img src={characterImg} /> : <></>}
+            </div>
+            ))}
+        </div>
     </div>
   )
 }
---
+++ src/characters.ts
@@ -0,0 +1,18 @@
+import { Character } from "./characterTypes";
+
+export const characters: Character[] = [{
+    tank: {
+      tank1: "../public/green.png",
+      tank2: "../public/orange.png",
+      tank3: "../public/three-eye.png",
+    }, dps: {
+      dps1: "../public/happy.png",
+    }, caster: {
+      caster1: "../public/orange-pink-hat.png",
+      caster2: "../public/blue-hat.png",
+      caster3: "../public/smirk.png",
+      caster4: "../public/unicorn.png"
+    }
+  },
+ 
+]
