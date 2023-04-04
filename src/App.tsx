d6575d32a6290b233bbd28049de9f47aad389989
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-04 15:30:36 +0200

    moving types, and ChampionPool component to seperate files

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
 import './App.css'
-import { characters } from './characters'
 
-
-
-
-
+import ChampionPool from './components/ChampionPool'
 
 
 function App() {
-
-  
-
-  const arr = []
-  
-  const characterImgs = []
-  for (const character of characters) {
-    for (const role of Object.values(character)) {
-      for (const image of Object.values(role)) {
-        if (image.endsWith(".png")) {
-          characterImgs.push(image)
-        }
-      }
-    }
-  }
-
-  console.log(characterImgs)
   return (
     <div className="App">
-        <h1>Autochess board</h1>
-        <div className='characters'>
-          {characterImgs.map((characterImg, idx) => (
-            <div key={idx} draggable={true} className='character-img'>
-              {characterImg ? <img src={characterImg} /> : <></>}
-            </div>
-            ))}
-        </div>
+        <ChampionPool />
     </div>
   )
 }
---
+++ src/components/ChampionPool.tsx
@@ -0,0 +1,28 @@
+import { characters } from '../characters'
+
+export default function ChampionPool() {
+    const characterImgs = []
+    for (const character of characters) {
+      for (const role of Object.values(character)) {
+        for (const image of Object.values(role)) {
+          if (image.endsWith(".png")) {
+            characterImgs.push(image)
+          }
+        }
+      }
+    }
+  
+    console.log(characterImgs)
+    return (
+      <div className="App">
+          <h1>Autochess board</h1>
+          <div className='characters'>
+            {characterImgs.map((characterImg, idx) => (
+              <div key={idx} draggable={true} className='character-img'>
+                {characterImg ? <img src={characterImg} /> : <></>}
+              </div>
+              ))}
+          </div>
+      </div>
+    )
+}
